fd5aca6d5cb78ecacffc966e721deb79a41da2c4
OvajStup <107275401+OvajStup@users.noreply.github.com>
2023-12-15 22:32:45 +0100

    day4 prototype

--- 4_scratchcards.py
+++ 4_scratchcards.py
@@ -10,32 +10,73 @@ def conversion(card:list):
     return card
 
 
-def getCardValue(line:str):
+def getCardValue(line:str, part):
     value = 0
     winning = line[9:].split(' | ')[0].split(' ')
     owned = line[9:].split(' | ')[1].split(' ')
     
     winning = conversion(winning)
     owned = conversion(owned)
+
+    if part == 1:
+        for win in winning:
+            if win in owned:
+                if value == 0:
+                    value = 1
+                else:
+                    value *= 2
+
+        return value
     
-    for win in winning:
-        if win in owned:
-            if value == 0:
-                value = 1
+    else:
+        won = 0
+        for win in winning:
+            if win in owned:
+                won += 1
+        
+        return 0
+    
+global memo
+memo = {}
+
+def cardCounting(cards, index):
+    won_now = getCardValue(cards[index])
+    if won_now == 0:
+        return 0
+    
+    else:
+        won = 0
+        for i in range(1, won_now + 1):
+            if index + i in memo:
+                return memo[index + i]
             else:
-                value *= 2
-    
-    return value
-       
+                won += cardCounting(cards, index + i) + 1
+                if index + 1 not in memo:
+                    memo[index + i] = won
+            
+        return won
+
 
 def main():
     fin = open(r'C:\Users\HP\OneDrive - fer.hr\UPRO\AoC\input_4.txt')
-    
-    card_sum = 0
-    for line in fin.readlines():
-        card_sum += getCardValue(line)
-        
+    part = 2
+
+    if part == 1:
+        card_sum = 0
+        for line in fin.readlines():
+            card_sum += getCardValue(line, part)
+
+    else:
+        cards = fin.readlines()
+        card_sum = 0
+        for i in range(len(cards)):
+            won = getCardValue(cards[i])
+            card_sum += cardCounting(cards, i)
+
+        card_sum += len(cards)
+
     print(card_sum)
+
 
 
 if __name__ == '__main__':
